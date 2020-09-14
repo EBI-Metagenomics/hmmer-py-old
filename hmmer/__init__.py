019cee3a5bbb831d4705f8121ecb74eb03b9b544
Danilo Horta <danilo.horta@pm.me>
2020-09-14 16:05:09 +0100

    setuptools_scm[toml]

--- hmmer/__init__.py
+++ hmmer/__init__.py
@@ -1,8 +1,14 @@
+from importlib import import_module as _import_module
+
 from . import typing
 from ._testit import test
-from ._version import __version__
 from .domtbl import read_domtbl
 from .hmmer import HMMER
 from .tbl import read_tbl
+
+try:
+    __version__ = getattr(_import_module("hmmer._version"), "version", "x.x.x")
+except ModuleNotFoundError:
+    __version__ = "x.x.x"
 
 __all__ = ["__version__", "test", "read_domtbl", "read_tbl", "HMMER", "typing"]
